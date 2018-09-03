be0f1990cf45a571e78870523b555327e8ef9392
Joanna Baran <joannabaran56@gmail.com>
2018-09-03 14:55:09 +0200

    Add Ingredient and Unit

--- src/main/java/com/codecool/krk/restcookbook/model/Ingredient.java
+++ src/main/java/com/codecool/krk/restcookbook/model/Ingredient.java
@@ -1,4 +1,41 @@
 package com.codecool.krk.restcookbook.model;
 
 public class Ingredient {
+
+    private String name;
+    private double amount;
+    private Unit unit;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String name, double amount, Unit unit) {
+        this.name = name;
+        this.amount = amount;
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
 }
