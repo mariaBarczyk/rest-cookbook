f471972306ed561b2951b9e1dfa19f243a5fa7da
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-03 15:44:48 +0200

    Add hibernate annotations to LoginData class

--- src/main/java/com/codecool/krk/restcookbook/model/LoginData.java
+++ src/main/java/com/codecool/krk/restcookbook/model/LoginData.java
@@ -1,8 +1,19 @@
 package com.codecool.krk.restcookbook.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "login_data")
 public class LoginData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "password", nullable = false)
     private String password;
 
     public LoginData() {
@@ -13,6 +24,15 @@ public class LoginData {
         this.password = password;
     }
 
+    public LoginData(Long id, String email, String password) {
+        this(email, password);
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
     public String getEmail() {
         return email;
     }
@@ -21,11 +41,24 @@ public class LoginData {
         return password;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginData{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
