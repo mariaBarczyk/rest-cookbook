77f12a37a24ecb8cf6de7ad3f46f067a619b6cec
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-03 17:10:03 +0200

    Add hibernate annotations to Ingredient class

--- src/main/java/com/codecool/krk/restcookbook/model/Ingredient.java
+++ src/main/java/com/codecool/krk/restcookbook/model/Ingredient.java
@@ -1,10 +1,26 @@
 package com.codecool.krk.restcookbook.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ingredient")
 public class Ingredient {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private double amount;
+
+    @Enumerated(EnumType.STRING)
     private Unit unit;
+
+    @ManyToOne
+    private Recipe recipe;
 
     public Ingredient() {
     }
@@ -13,6 +29,19 @@ public class Ingredient {
         this.name = name;
         this.amount = amount;
         this.unit = unit;
+    }
+
+    public Ingredient(Long id, String name, double amount, Unit unit) {
+        this(name, amount, unit);
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
